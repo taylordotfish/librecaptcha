59c5a62e725a9b63a6678fd7589f67749e4762ed
taylor.fish <contact@taylor.fish>
2018-02-18 02:05:54 -0800

    Make installable; v0.3.3

--- librecaptcha/librecaptcha.py
+++ librecaptcha/librecaptcha.py
@@ -32,7 +32,7 @@ import re
 import sys
 import time
 
-__version__ = "0.2.2"
+__version__ = "0.3.3"
 
 BASE_URL = "https://www.google.com/recaptcha/api2/"
 API_JS_URL = "https://www.google.com/recaptcha/api.js"
