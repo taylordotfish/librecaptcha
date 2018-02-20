09d38f171ab2fe89fa8e22fadfe2643e4d19c71b
taylor.fish <contact@taylor.fish>
2018-02-19 17:38:25 -0800

    Bump dev version

--- librecaptcha/librecaptcha.py
+++ librecaptcha/librecaptcha.py
@@ -32,7 +32,7 @@ import re
 import sys
 import time
 
-__version__ = "0.3.4"
+__version__ = "0.3.5-dev"
 
 BASE_URL = "https://www.google.com/recaptcha/api2/"
 API_JS_URL = "https://www.google.com/recaptcha/api.js"
