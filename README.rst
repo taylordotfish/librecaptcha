515f7359b8e46bf78b68de389eb1f1523229ee9e
nickolas360 <contact@nickolas360.com>
2019-04-05 00:34:07 -0700

    Version 0.6.1

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 librecaptcha
 ============
 
-Version 0.6.0
+Version 0.6.1
 
 librecaptcha is a free/libre program and library that allows you to solve
 `reCAPTCHA`_ challenges.
--- librecaptcha/librecaptcha.py
+++ librecaptcha/librecaptcha.py
@@ -19,7 +19,7 @@ from . import cli
 from .errors import GtkImportError, UserError
 from .recaptcha import ReCaptcha
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 
 def _get_gui():
