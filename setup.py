2d7dc7b84883b5f329393fac8f8496017f3232ed
taylor.fish <contact@taylor.fish>
2019-04-05 00:34:07 -0700

    Version 0.6.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)\
 
 setup(
     name="librecaptcha",
-    version="0.6.0",
+    version="0.6.1",
     description="A free/libre interface for solving reCAPTCHA challenges.",
     long_description=long_description(),
     url="https://github.com/taylordotfish/librecaptcha",
